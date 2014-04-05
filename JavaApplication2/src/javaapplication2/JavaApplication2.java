f5366e61407363aaad11e604cfc3564eac426d40
Marine <Marine@marine-pc>
2014-04-05 14:25:28 +0200

    version avec la récupération des données : une liste des rues et une liste des intersections, version corrigée

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -24,90 +24,36 @@ public class JavaApplication2 {
     /**
      * @param args the command line arguments
      */
+   ArrayList<Rue> ruesMain;
+   ArrayList<Intersection> intersectionsMain; 
+   
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-         
+        
+        ArrayList liste = new ArrayList();
+        ArrayList<Rue> rues = new ArrayList<Rue>();
+        ArrayList<Intersection> intersections = new ArrayList<Intersection>();
+        
+        int nbre_intersections = 11348;
+        int nbre_rues = 17958;
+        int nbre_secondes = 54000;
+        int nbre_véhicules = 8;
+        int intersection_depart = 4516;
+
         // On lit le fichier
-        lectureFichier();
+        Utils utils = new Utils();
+        liste = utils.lectureFichier();
+
+        for(int i = 0; i<nbre_intersections;i++)
+        {
+            intersections.add((Intersection)liste.get(i));
+            System.out.println(intersections.get(i));
+        }
+        
+        for(int j = nbre_intersections+1; j<liste.size();j++)
+        {
+            rues.add((Rue)liste.get(j));
+        }
     }
     
-    // Lecture du fichier
-    public static void lectureFichier()
-    {
-         
-         // Les variables utilisées pour le pb
-         int nbre_intersections = 11348;
-         int nbre_rues = 17958;
-         int nbre_secondes = 54000;
-         int nbre_véhicules = 8;
-         int intersection_depart = 4516;
-         
-         // Liste de rue
-         ArrayList<Rue> rues = new ArrayList<Rue>();
-         
-         // Liste d'intersections
-         ArrayList<Intersection> intersections = new ArrayList<>();
-         
-         String fichier = "C:\\Users\\Marine\\Desktop\\paris_54000.txt";
-         String ligne = "";
-         int num_ligne = 0;
-         
-         String[] groupes_ligne;
-         
-         Rue rue;
-         long latitude;
-         long longitude;
-         
-         Intersection intersection;
-         int intDepart;
-         int intArrivee;
-         int sens;
-         int cout;
-         int longueur;
-         
-	BufferedReader ficTexte;
-	try {
-            ficTexte = new BufferedReader(new FileReader(new File(fichier)));
-            if (ficTexte == null) {
-		throw new FileNotFoundException("Fichier non trouvé: "+ fichier);
-            }
-            do {
-		ligne = ficTexte.readLine();
-                if (ligne != null) {
-                    //System.out.println(ligne);
-                    if(num_ligne!=0)
-                    {
-                        if(num_ligne<nbre_intersections)
-                        {
-                            // On stocke le nombre de rues
-                            groupes_ligne = ligne.split(" ");
-                            latitude = Long.parseLong(groupes_ligne[0]);
-                            longitude = Long.parseLong(groupes_ligne[1]);
-                            intersection = new Intersection(latitude, longitude);
-                            intersections.add(intersection);
-                        }
-                        else
-                        {
-                            // On stocke le nombre d'intersections
-                            groupes_ligne = ligne.split(" ");
-                            intDepart = Integer.parseInt(groupes_ligne[0]);
-                            intArrivee = Integer.parseInt(groupes_ligne[1]);
-                            sens = Integer.parseInt(groupes_ligne[2]);
-                            cout = Integer.parseInt(groupes_ligne[3]);
-                            longueur = Integer.parseInt(groupes_ligne[4]);
-                            rue = new Rue(intDepart, intArrivee, sens, cout, longueur);
-                            rues.add(rue);
-                        }
-                    }
-                    num_ligne++;
-		}
-            } while (ficTexte != null);
-            ficTexte.close();
-	} catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-	} catch (IOException e) {
-            System.out.println(e.getMessage());
-	}
-    }
     
 }
